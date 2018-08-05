944fd7b743c81b2f514c3d443284cdc7061874cb
Billy Derringer <billyderringer7@gmail.com>
2018-08-05 18:50:00 -0500

    added splash bg

---
+++ config.js
@@ -0,0 +1,9 @@
+requirejs.config({
+    paths: {
+        jquery: [
+            'https://code.jquery.com/jquery-3.3.1.min',
+            'js/jQuery-3.3.1.min'
+        ],
+        styles: 'js/styles'
+    }
+})
---
+++ js/styles.js
@@ -0,0 +1,17 @@
+define(['jquery'], () => {
+
+    //nav scroll bg effect
+    $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+            $("#floating-nav").css({
+                "background-color": "#000",
+                "transition": "1s"
+            }); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+            $("#floating-nav").css({
+                "background-color": "transparent",
+                "transition": "1s"
+            }); // if not, change it back to transparent
+        }
+    })
+})
