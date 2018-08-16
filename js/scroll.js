1c827b62d8ea60adeaf6b070fc71daeb1e19caa2
Billy Derringer <billyderringer7@gmail.com>
2018-08-16 01:13:10 -0500

    added smooth scroll to most links

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+define(['jquery'], () => {
+    $(function () {
+        // This will select everything with the class smoothScroll
+        // This should prevent problems with carousel, scrollspy, etc...
+        $('.smoothScroll').click(function () {
+            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+                location.hostname === this.hostname) {
+                let target = $(this.hash)
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 800) // The number here represents the speed of the scroll in milliseconds
+                    return false
+                }
+            }
+        })
+    })
+})
